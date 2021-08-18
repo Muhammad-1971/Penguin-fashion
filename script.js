0d12737b465574a16044b012634c58092f95bc3a
Muhammad-1971 <shohanurislam1971@gmail.com>
2021-08-17 23:52:30 -0800

    js file add

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('penguinButton').addEventListener('click',function(){
+  const penguinChangeText = document.getElementById('changeText');
+  penguinChangeText.innerText ="I am PENGUIN";
+  penguinChangeText= '';
+})
